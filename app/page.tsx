211f4e8a2e8eb9fb0fa8cae8d422ddac4a29812b
kevinnaserwan <kevinnaserwan@gmail.com>
2024-07-12 14:07:34 +0700

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <main className=" relative flex min-h-screen flex-col items-center  overflow-clip mx-auto   bg-blue-500">
+    <main className=" relative flex min-h-screen flex-col items-center  overflow-auto overflow-y-hidden mx-auto   bg-blue-500">
       <Hero />
       <div className=" bg-white min-h-screen w-full flex flex-col justify-start pt-12 items-center">
         <Navbar />
