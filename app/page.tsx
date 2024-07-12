fb7426ce80eefdf5810634df6d733dc272d86e3a
kevinnaserwan <kevinnaserwan@gmail.com>
2024-07-12 14:04:22 +0700

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className=" relative flex min-h-screen flex-col items-center  overflow-clip mx-auto   bg-blue-500">
       <Hero />
-      <div className=" bg-white max-h-screen w-full flex flex-col justify-start pt-12 pb-6 items-center">
+      <div className=" bg-white min-h-screen w-full flex flex-col justify-start pt-12 pb-6 items-center">
         <Navbar />
         <Main />
       </div>
