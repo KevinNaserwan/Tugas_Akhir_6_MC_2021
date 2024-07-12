8579c55857e0573925f404803a117d008ba72b0f
kevinnaserwan <kevinnaserwan@gmail.com>
2024-07-12 13:42:53 +0700

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <main className=" relative flex min-h-screen flex-col items-center justify-center overflow-clip mx-auto lg:p-24 p-10">
+    <main className=" relative flex min-h-screen flex-col items-center justify-center overflow-clip mx-auto lg:p-24 px-10 py-3">
       <Hero />
       <Navbar />
       <Main />
